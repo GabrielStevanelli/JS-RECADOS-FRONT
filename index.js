2d8094c462d42b0abf9698e02ce1d2459e6d036a
Gabriel Stevanelli <Gabriel_stevanelli@hotmail.com>
2021-11-21 14:40:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,89 +1,98 @@
-document.getElementById('adiciona').onclick=adiciona;
+const api = axios.create({
+  baseURL: "https://lista-recados-cache.herokuapp.com/",
+});
+
+document.getElementById("adiciona").onclick = adiciona;
 let mensagens = [];
 
-async function getMessage(){
-    const {data: mensagem} = await axios.get('https://banco-api.herokuapp.com/listar')
+async function getMessage() {
+  const { data } = await api.get("/messages");
 
-    mensagens = mensagem;
-    geraLista();
+  mensagens = data;
+  geraLista();
 }
 
-async function adiciona(){
-    let titulo = document.getElementById("titulo").value;
-    let mensagem = document.getElementById("mensagem").value;
+async function adiciona() {
+  let titulo = document.getElementById("titulo").value;
+  let mensagem = document.getElementById("mensagem").value;
 
-    if(titulo == '' || mensagem == ''){
-        return;
-    }
+  if (titulo == "" || mensagem == "") {
+    return;
+  }
 
-    const {data: {id,title,message}} = await axios.post('https://banco-api.herokuapp.com/adicionar',{title: titulo,message: mensagem})
-    mensagens.push({id,title,message});
-    getMessage()
-    
+  const {
+    data: { uid, title, message },
+  } = await api.post("/messages", {
+    title: titulo,
+    message: mensagem,
+  });
+  mensagens.push({ uid, title, message });
+  getMessage();
 }
 
 /*let mensagens = JSON.parse(localStorage.getItem("mensagens")) || [];*/
 
-function geraLista(){
-    let tBody = document.getElementById("tbody");
-    tBody.innerHTML = "";
-    for(let message of mensagens){
+function geraLista() {
+  let tBody = document.getElementById("tbody");
+  tBody.innerHTML = "";
+  for (let message of mensagens) {
+    let tr = document.createElement("tr");
+    let th = document.createElement("th");
+    let tdTitulo = document.createElement("td");
+    let tdMensagem = document.createElement("td");
+    let tdGerenciar = document.createElement("td");
 
-        let tr = document.createElement("tr");
-        let th = document.createElement("th");
-        let tdTitulo = document.createElement("td");
-        let tdMensagem = document.createElement("td");
-        let tdGerenciar = document.createElement("td");
+    let deleteBtn = document.createElement("button");
+    deleteBtn.setAttribute("class", "btn btn-danger mx-2");
+    deleteBtn.setAttribute("id", message.uid);
+    deleteBtn.onclick = async function () {
+      await api.delete(`/messages/${message.uid}`);
+      getMessage();
+    };
+    deleteBtn.innerHTML = "Excluir";
 
-        let deleteBtn = document.createElement("button");
-        deleteBtn.setAttribute("class","btn btn-danger mx-2");
-        deleteBtn.setAttribute("id",message.id);
-        deleteBtn.onclick = async function () {
-            await axios.delete(`https://banco-api.herokuapp.com/apaga/${message.id}`)
-            getMessage()
-        };
-        deleteBtn.innerHTML = "Excluir"
+    let editBtn = document.createElement("button");
+    editBtn.setAttribute("class", "btn btn-warning mx-2");
+    editBtn.setAttribute("id", "myBtn");
+    editBtn.onclick = function () {
+      modal.style.display = "block";
+      let editaModalBtn = document.getElementById("editaModal");
 
-        let editBtn = document.createElement("button");
-        editBtn.setAttribute("class","btn btn-warning mx-2");
-        editBtn.setAttribute("id","myBtn");
-        editBtn.onclick = function() {
-            modal.style.display = "block";
-            let editaModalBtn = document.getElementById("editaModal");
+      document.getElementById("tituloModal").value = message.title;
+      document.getElementById("mensagemModal").value = message.message;
 
-            document.getElementById("tituloModal").value = message.title;
-            document.getElementById("mensagemModal").value = message.message;
+      editaModalBtn.onclick = async function () {
+        let titulo = document.getElementById("tituloModal").value;
+        let mensagem = document.getElementById("mensagemModal").value;
+        await api.put(`/messages/${message.uid}`, {
+          title: titulo,
+          message: mensagem,
+        });
+        getMessage();
+      };
+    };
+    editBtn.innerHTML = "Editar";
 
-            editaModalBtn.onclick = async function () {
-                let titulo = document.getElementById("tituloModal").value;
-                let mensagem = document.getElementById("mensagemModal").value 
-                await 
-                axios.put(`https://banco-api.herokuapp.com/mudar/${message.id}`,{title: titulo,message: mensagem});
-                getMessage()
-            }
-        }
-        editBtn.innerHTML = "Editar"
+    tdGerenciar.appendChild(editBtn);
+    tdGerenciar.appendChild(deleteBtn);
 
-        tdGerenciar.appendChild(editBtn);
-        tdGerenciar.appendChild(deleteBtn);
+    th.innerHTML = message.uid;
+    tdTitulo.innerHTML = message.title;
+    tdMensagem.innerHTML = message.message;
 
-        th.innerHTML = message.id;
-        tdTitulo.innerHTML = message.title;
-        tdMensagem.innerHTML = message.message;
+    tr.appendChild(th);
+    tr.appendChild(tdTitulo);
+    tr.appendChild(tdMensagem);
+    tr.appendChild(tdGerenciar);
 
-        tr.appendChild(th);
-        tr.appendChild(tdTitulo);
-        tr.appendChild(tdMensagem);
-        tr.appendChild(tdGerenciar);
-
-        tBody.appendChild(tr);
-    }
+    tBody.appendChild(tr);
+  }
 }
 
 getMessage();
 
-function salvaLocalStorage(){
-    localStorage.setItem('mensagens',JSON.stringify(mensagens));
+function salvaLocalStorage() {
+  localStorage.setItem("mensagens", JSON.stringify(mensagens));
 }
 
 // Get the modal
@@ -96,13 +105,13 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
